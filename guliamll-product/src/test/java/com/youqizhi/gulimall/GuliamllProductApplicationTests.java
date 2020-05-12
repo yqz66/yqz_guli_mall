1fd5a2f9dd31413913311ea6a6f3050467e4b2ba
yqz66 <89452898@qq.com>
2020-05-12 20:51:35 +0800

    项目初始结构创建

---
+++ guliamll-product/src/test/java/com/youqizhi/gulimall/GuliamllProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youqizhi.gulimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliamllProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
