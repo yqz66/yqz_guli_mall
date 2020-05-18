c53a7cafe7b0ab790eafedc70181a96bbb40d42f
yqz66 <89452898@qq.com>
2020-05-18 20:27:47 +0800

    集成nacos配置中心

--- gulimall-product/src/main/java/com/youqizhi/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/youqizhi/gulimall/product/GulimallProductApplication.java
@@ -3,10 +3,13 @@ package com.youqizhi.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @MapperScan("com.youqizhi.gulimall.product.dao")
+//开启服务注册与发现
+@EnableDiscoveryClient
 public class GulimallProductApplication {
 
 	public static void main(String[] args) {
